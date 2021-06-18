5e0658495a87d47cb2fcdbf12fe4fd0ea71da769
Goran Novakovic <gnovakov@gmail.com>
2021-06-18 22:51:52 +0100

    navigation set up

--- app/src/main/java/com/gnova/breakingnewsapp/MainActivity.kt
+++ app/src/main/java/com/gnova/breakingnewsapp/MainActivity.kt
@@ -2,10 +2,21 @@ package com.gnova.breakingnewsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.gnova.breakingnewsapp.databinding.ActivityMainBinding
+
+private lateinit var binding: ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.bottomNavigationView.setupWithNavController(findNavController(R.id.NavHostFragment))
+
     }
 }
---
+++ app/src/main/java/com/gnova/breakingnewsapp/ui/article/ArticleFragment.kt
@@ -0,0 +1,9 @@
+package com.gnova.breakingnewsapp.ui.article
+
+import androidx.fragment.app.Fragment
+import com.gnova.breakingnewsapp.R
+
+class ArticleFragment : Fragment(R.layout.fragment_article) {
+
+
+}
