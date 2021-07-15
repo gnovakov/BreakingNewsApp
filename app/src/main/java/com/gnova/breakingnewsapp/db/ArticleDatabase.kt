efa3cac9b05e7f255b223cc4ea2cf18be41b8d2a
Goran Novakovic <gnovakov@gmail.com>
2021-07-15 17:57:56 +0100

    Moved ArticleDatabase to app module

--- app/src/main/java/com/gnova/breakingnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/gnova/breakingnewsapp/db/ArticleDatabase.kt
@@ -1,7 +1,8 @@
-package com.gnova.data.db
+package com.gnova.breakingnewsapp.db
 
 import androidx.room.Database
 import androidx.room.RoomDatabase
+import com.gnova.data.db.ArticleDao
 import com.gnova.data.models.ArticleDTO
 
 @Database(entities = [ArticleDTO::class], version = 1, exportSchema = false)
---
+++ domain/src/main/java/com/gnova/domain/models/Source.kt
@@ -0,0 +1,8 @@
+package com.gnova.domain.models
+
+import java.io.Serializable
+
+data class Source(
+        val id: Any? = "",
+        val name: String? = ""
+) : Serializable
