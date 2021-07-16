deec9e04f8b2d837a91669e221407262e7d9fca5
Goran Novakovic <gnovakov@gmail.com>
2021-07-16 17:28:50 +0100

    Room Ratabase Class finished

--- app/src/main/java/com/gnova/breakingnewsapp/db/ArticleDatabase.kt
+++ app/src/main/java/com/gnova/breakingnewsapp/db/ArticleDatabase.kt
@@ -1,6 +1,8 @@
 package com.gnova.breakingnewsapp.db
 
+import android.content.Context
 import androidx.room.Database
+import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.gnova.data.db.ArticleDao
@@ -13,4 +15,25 @@ import com.gnova.data.models.ArticleDTO
 abstract class ArticleDatabase: RoomDatabase() {
 
     abstract fun articleDao(): ArticleDao
+
+    companion object {
+        @Volatile
+        private var INSTANCE: ArticleDatabase? = null
+
+        fun getDatabase(context: Context): ArticleDatabase {
+            val tempInstance = INSTANCE
+            if (tempInstance != null) {
+                return tempInstance
+            }
+            synchronized(this) {
+                val instance = Room.databaseBuilder(
+                    context.applicationContext,
+                    ArticleDatabase::class.java,
+                    "article_database"
+                ).build()
+                INSTANCE = instance
+                return instance
+            }
+        }
+    }
 }
